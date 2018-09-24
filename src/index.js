f6a7771e46436f45d0b890d0b56fdbfc8838b8a8
Yulia <43168694+feulia@users.noreply.github.com>
2018-09-24 18:12:18 +0300

    Finish the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  var int = equation.split(' ');
+  var int1 = parseInt(int[0], 10);
+  var int2 = parseInt(int[3] + int[4], 10);
+  var int3 = parseInt(int[7] + int[8], 10);
+  var eq = Math.pow(int2, 2) - 4 * int1 * int3;
+  
+  var members = [
+    (Math.round((int2 * - 1 + Math.sqrt(eq)) / (int1 * 2))),
+    (Math.round((int2 * - 1 - Math.sqrt(eq)) / (int1 * 2)))
+  ];
+  
+  return members.sort((a, b) => { a - b });
 }
